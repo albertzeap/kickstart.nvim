aed8000e833c52be5e705534ebc213323b93381f
Albert Paez <albertzeap@gmail.com>
2025-12-23 16:29:44 -0800

    Add reselect of visual block when indenting

--- init.lua
+++ init.lua
@@ -195,6 +195,9 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
+-- Reselect visual block when indenting
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
